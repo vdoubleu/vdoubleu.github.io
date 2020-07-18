b0ed11b3a812ec60b1737579990c4cbc32a1fdc9
Victor Wang <victorwang2001@gmail.com>
2020-07-18 10:59:30 -0400

    replaced website

---
+++ precache-manifest.3901c66f7ce030d185238b71532ea582.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19f99a4b220f33c808efd4c113494bf9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a41383e5daeb7b977c93",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b1f2a203712035b3461d",
+    "url": "/static/css/main.21f5180f.chunk.css"
+  },
+  {
+    "revision": "a41383e5daeb7b977c93",
+    "url": "/static/js/2.03d11bae.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.03d11bae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1f2a203712035b3461d",
+    "url": "/static/js/main.309e4412.chunk.js"
+  },
+  {
+    "revision": "8da32427f6dff9b5abfa",
+    "url": "/static/js/runtime-main.99e80802.js"
+  },
+  {
+    "revision": "ba5dd1b0c781a361ff3d2da876e1868a",
+    "url": "/static/media/code.ba5dd1b0.png"
+  },
+  {
+    "revision": "d58758c84e5b1f17783cbba4732a3e6f",
+    "url": "/static/media/cosmo.d58758c8.png"
+  },
+  {
+    "revision": "a47b97fa8f2141669e1c5d939cb4c307",
+    "url": "/static/media/cubetimer.a47b97fa.png"
+  },
+  {
+    "revision": "94ce9bb2a8d01b8239806a91a9e17658",
+    "url": "/static/media/inform.94ce9bb2.png"
+  },
+  {
+    "revision": "f2a45a73108630d1d70a86a98434a013",
+    "url": "/static/media/math_equation_solver.f2a45a73.png"
+  },
+  {
+    "revision": "97f90aa3f89ea742173207e98a7299c9",
+    "url": "/static/media/me.97f90aa3.JPG"
+  },
+  {
+    "revision": "a51d52f43fb9dcedb9702b9ec8d1ed81",
+    "url": "/static/media/neuralnet.a51d52f4.png"
+  },
+  {
+    "revision": "8ea902a77a99951eab99a48f55a07d5d",
+    "url": "/static/media/ngram.8ea902a7.png"
+  },
+  {
+    "revision": "394d9e356ccdee6f383da4d85948da28",
+    "url": "/static/media/rob1.394d9e35.jpg"
+  },
+  {
+    "revision": "41ca40f4f9eaa8c93bff7ffe0148dfc9",
+    "url": "/static/media/robotPrediction.41ca40f4.png"
+  },
+  {
+    "revision": "7f37a5ae5a02bba8215adb4d66e2ff6c",
+    "url": "/static/media/robotics.7f37a5ae.jpg"
+  },
+  {
+    "revision": "7ff764408ca8c65d20405354e6f2def6",
+    "url": "/static/media/set.7ff76440.jpg"
+  },
+  {
+    "revision": "8fc59e822d9cdf1d85157041ef2601f6",
+    "url": "/static/media/set2.8fc59e82.jpg"
+  },
+  {
+    "revision": "1cc14ec3fc06d14cb5fd7256926de1d0",
+    "url": "/static/media/sethacks-site.1cc14ec3.png"
+  },
+  {
+    "revision": "1d2e4ca66c8ced257e186827d705582a",
+    "url": "/static/media/turr.1d2e4ca6.jpg"
+  }
+]);
